051ea79a24621115de091371ad97edf022c1443f
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-01-16 16:29:29 +0800

    feat(instancemanager): add a user ID getter.
    Add a function which get the user's ID which run the process, which is
    ready to be one part of QLocalServer name.

--- src/instancemanager.cpp
+++ src/instancemanager.cpp
@@ -3,6 +3,58 @@
 #include <QApplication>
 #include <QByteArray>
 
+#ifdef Q_OS_WIN
+// clang-format off
+#include <windows.h>
+#include <sddl.h>
+// clang-format on
+#else
+#include <unistd.h>
+#endif
+
+namespace {
+    QString _getCurrentUserIdString() {
+#ifdef Q_OS_WIN
+        HANDLE hToken = nullptr;
+        if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken)) {
+            return {};
+        }
+
+        // Get the required size of the buffer
+        DWORD cbSize = 0;
+        GetTokenInformation(hToken, TOKEN_INFORMATION_CLASS::TokenUser, nullptr, 0, &cbSize);
+        if (GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
+            CloseHandle(hToken);
+            return {};
+        }
+
+        QByteArray buffer(cbSize, 0);
+        if (!GetTokenInformation(hToken, TOKEN_INFORMATION_CLASS::TokenUser, buffer.data(), cbSize, &cbSize)) {
+            CloseHandle(hToken);
+            return {};
+        }
+
+        TOKEN_USER* tokenUser = reinterpret_cast<TOKEN_USER*>(buffer.data());
+        LPSTR sidString = nullptr;
+
+        if (!ConvertSidToStringSidA(tokenUser->User.Sid, &sidString)) {
+            CloseHandle(hToken);
+            return {};
+        }
+
+        QString sid = QString::fromLocal8Bit(sidString);
+        LocalFree(sidString);
+        CloseHandle(hToken);
+        return sid; // e.g., "S-1-5-21-1234567890-1234567890-1234567890-1001"
+
+#else
+#warning Untested
+        // TODO I don't have a Linux desktop environment and macOS, so actually I have never test this part of code.
+        return QString::number(getuid(), 10);
+#endif
+    }
+} // namespace
+
 InstanceManager InstanceManager::instance;
 
 InstanceManager& InstanceManager::getInstance() {
